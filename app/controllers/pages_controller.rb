715aeac150b6ef3d3c65c4a0bea5762ce691c599
Juanma Binda <juanmanuelbinda@gmail.com>
2023-05-29 15:55:30 -0300

    Done

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,15 @@
+class PagesController < ApplicationController
+  def ask
+    @question = params[:ask]
+  end
+
+  def answer
+    if params[:ask] == 'I am going to work'
+      @answer = 'Great!'
+    elsif params[:ask].ends_with?('?')
+      @answer = 'Silly question, get dressed and go to work!'
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
+  end
+end
